648d1f58a1a9602f9f1c34815cfe7be3aefc4762
gkumar <govind.kumar@coalfire.com>
2024-11-13 18:09:07 +0530

    Update aws version

--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.75.1"
+      version = ">= 5.70.0"
     }
     helm = {
       source  = "hashicorp/helm"
