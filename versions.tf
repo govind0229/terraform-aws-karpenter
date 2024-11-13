cb7aecde897c9b9b56e0dc8bf0d10bd35a6ecd29
gkumar <govind.kumar@coalfire.com>
2024-11-13 18:27:46 +0530

    Update aws version

--- versions.tf
+++ versions.tf
@@ -1,19 +1,23 @@
 // Required Terraform version
 terraform {
-  required_version = ">= 1.7.0"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.70.0"
+      version = ">= 4.19.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.16.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.16.1"
+      version = ">= 2.6.0"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = ">= 3.4.5"
+    utils = {
+      source  = "cloudposse/utils"
+      version = ">= 0.17.0"
     }
   }
 }
